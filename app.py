6333dded9549b3c3fe05aabcbcb687e92fac2fb8
Dan Roth <rdan689@gmail.com>
2020-02-20 12:15:05 -0500

    contact finder enhancements, some bugs fixed

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ def main():
     listing_url = input("Welcome to Recruit_Link!  Please enter a job listing's LinkedIn url:\n")
     name, title, profile, company, company_website, job_title = \
         find_contact(listing_url, linkedin_username, linkedin_passkey, chromedriver_dir)
-    company_website = company_website.replace("bit.ly", f"{company}.com")
+    company_website = company_website.replace("bit.ly", f"{company}.com").replace("/careers", "")
     print(f"Now searching for: {name.strip()}, {title.strip()}\n")
 
     email_message = f"Hi {name.split()[0]},\n" \
               f"\n" \
               f"I hope this email finds you well.  I just recently saw a posting for a {job_title} position at {company}" \
-              f" and would love to learn more.  My experience SKILLS HERE feels like a great fit for the role.\n" \
+              f" and would love to learn more.  My experience with Python, SQL, SKILLS HERE feels like a great fit for " \
+                    f"the role.\n" \
               f"\n" \
               f"Could you tell me more about the data science team and what it's like to work for the company?  " \
               f"I believe you would have great insight as a {title.strip()} for {company}.\n" \
@@ -34,22 +35,24 @@ def main():
               f"\n"
 
     linkedin_message = f"Hi {name.split()[0]}, I recently saw a {job_title} position at {company} and I wanted " \
-                       f"to learn more about your company! My experience with FILL feels like a great fit for the " \
-                       f"role. Would love any more information you have, thanks!"
+                       f"to learn more about your company! My experience with Python, SQL, FILL feels like a great fit " \
+                       f"for the role. Would love any more information you have, thanks!"
 
     name = name.lower()
     company_email = company_website.replace("https://www.", "@")\
                                    .replace("http://www.", "@")\
                                    .replace("http:", "@")\
+                                   .replace("https:", "@")\
+                                   .replace("jobs.", "")\
                                    .replace(r"\.com.*$", ".com")\
                                    .replace("/", "")
     patterns = ["finitiallast", "finitial.last", "firstlast", "first.last",
                 "first", "last", "firstlinitial", "first.linitial", "first_linitial", "finitial_last"]
     patterns = [pattern
                     .replace("first", name.split()[0])
-                    .replace("last", name.split()[1])
+                    .replace("last", name.split()[-1])
                     .replace("finitial", name.split()[0][0])
-                    .replace("linitial", name.split()[1][0]) + company_email for pattern in patterns]
+                    .replace("linitial", name.split()[-1][0]) + company_email for pattern in patterns]
 
     hunter = PyHunter(hunter_apikey)  # Hunter API Key here
     hunter_email, confidence_score = hunter.email_finder(company_website, full_name=name)
